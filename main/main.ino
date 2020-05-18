c68a7f064bc08f85dade374d4c63040b33d8b6bb
Doc-Hu <61994806+Doc-Hu@users.noreply.github.com>
2020-05-18 13:05:24 +1200

    added behaviour

---
+++ main/main.ino
@@ -0,0 +1,169 @@
+// Project 2 (Firefighter project) for MechEng706 Mechatronics design
+// Authors (Group 21):  Narada Hu, nhu769
+//                   :
+//                   :
+//                   :
+
+// This is the main file for controlling the vex robot. It includes the
+// main behaviour control system and calls upon supporting functions and
+// files in the libraries. The purpose of the program is to control a vex
+// robot to extinguish fires when placed in a walled area with unknown
+// obstacles and fire locations. Behaviour and fuzzy logic control are used
+// to implement this system.
+
+
+//----------------------Libraries------------------------------------------------------------------------------------------------------------------------------------------
+// include libraries
+#include "libraries\Controllers\Controllers.h"
+#include "libraries\Controllers\Controllers.cpp"
+#include "libraries\Sensors\Sensors.h"
+#include "libraries\Sensors\Sensors.cpp"
+#include "libraries\Motors\Motors.h"
+#include "libraries\Motors\Motors.cpp"
+
+//Initializing global objects
+Controllers controlSystem;
+Sensors sensor;
+Motors motor;
+// might have fused these filters with sensor library
+//Kalman kalmanX;
+//Kalman kalmanY;
+
+//----------------------Battery check and Serial Comms---------------------------------------------------------------------------------------------------------------------
+//Serial Pointer
+HardwareSerial *SerialCom;
+//----------------------Setup------------------------------------------------------------------------------------------------------------------------------------------
+
+// Enum for motion states
+enum MOTION {
+  FORWARD,
+  BACKWARD,
+  STRAFE_LEFT,
+  STRAFE_RIGHT,
+  LEFT_TURN,
+  RIGHT_TURN,
+  LEFT_ARC,
+  RIGHT_ARC,
+  BACKWARD_LEFT_TURN,
+  STOP,
+};
+
+
+// Declare commands and command flags
+MOTION halt_command;
+int halt_output_flag;
+MOTION extinguish_command;
+int extinguish_output_flag;
+MOTION avoid_command;
+int avoid_output_flag;
+MOTION moveToFire_command;
+int moveToFire_output_flag;
+MOTION locate_command;
+int locate_output_flag;
+
+MOTION motor_input;
+
+// seems thay have declared command twice?
+// int cruise_command;
+// int cruise_output_flag;
+// int extinguish_command;
+// int extinguish_output_flag;
+// int avoid_command;
+// int avoid_output_flag;
+// int moveToFire_command;
+// int moveToFire_output_flag;
+
+void halt() {
+  halt_command = STOP;
+  halt_output_flag = 1;
+}
+
+
+void avoid() {
+  intval;
+  val = ir_detect();
+  if (val == 0b11) {
+    avoid_output_flag = 1;
+    avoid_command = BACKWARD;
+  }
+  else if (val == 0b10) {
+    avoid_output_flag = 1;
+    avoid_command = RIGHT_ARC;
+  } else if (val == 0b01) {
+    avoid_output_flag = 1;
+    avoid_command = LEFT_ARC;
+  } else {
+    avoid_output_flag = 0;
+  }
+}
+
+
+void extinguish() {
+  int left_photo, right_photo, delta;
+  left_photo = analog(1);
+  right_photo = analog(0);
+  delta = right_photo - left_photo;
+  if (abs(delta) > photo_dead_zone) {
+    if (delta > 0)extinguish_command = LEFT_TURN;
+    else extinguish_command = RIGHT_TURN;
+    extinguish_output_flag = 1;
+  } else  extinguish_output_flag = 0;
+}
+
+void locate() {
+  int left_photo, right_photo, delta;
+  left_photo = analog(1);
+  right_photo = analog(0);
+  delta = right_photo - left_photo;
+  if (abs(delta) > photo_dead_zone) {
+    if (delta > 0)locate_command = LEFT_TURN;
+    else locate_command = RIGHT_TURN;
+    locate_output_flag = 1;
+  } else  locate_output_flag = 0;
+}
+
+void moveToFire() {
+  bumper_check();
+  if (bumper_left && bump_right) {
+    moveToFire_output_flag = 1;
+    moveToFire_command = BACKWARD;
+    sleep (0.2);
+    moveToFire_command = LEFT_TURN;
+    sleep (0.4);
+  } else if (bumper_left) {
+    moveToFire_output_flag = 1;
+    moveToFire_command = RIGHT_TURN;
+    sleep(0.4);
+  } else if (bump_right) {
+    moveToFire_output_flag = 1;
+    moveToFire_command = LEFT_TURN;
+    sleep (0.4);
+  } else if (bump_back) {
+    moveToFire_output_flag = 1;
+    moveToFire_command = LEFT_TURN;
+    sleep(0.2);
+  } else  moveToFire_output_flag = 0
+}
+
+void Suppressor() {
+  if (locate_output_flag == 1) {
+    motor_input = locate_output;
+  } if (moveToFire_output_flag == 1) {
+    motor_input = moveToFire_output;
+  } if (avoid_output_flag == 1) {
+    motor_input = avoid_output;
+  } if (extinguish_output_flag == 1) {
+    motor_input = extinguish_output;
+  } if (halt_output_flag == 1) {
+    motor_input = halt_output;
+  } sleep(tick);
+}
+
+void main () {
+  start_process(motor_driver());
+  start_process(cruise());
+  start_process(extinguish());
+  start_process(avoid());
+  start_process(moveToFire());
+  start_process(arbitrate());
+}
